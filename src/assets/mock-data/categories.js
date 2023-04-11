fb2aee2d1511de018d292740ecc5a1c4baa8d1a8
Shubham Phadte <shubhamphadte37@gmail.com>
2023-04-12 00:10:17 +0530

    Added assets, mock data and images

---
+++ src/assets/mock-data/categories.js
@@ -0,0 +1,19 @@
+export const categories = [
+  { categoryId: 0, categoryName: "All", value: "all" },
+  { categoryId: 1, categoryName: "Babies & Kids", value: "babies-and-kids" },
+  { categoryId: 2, categoryName: "Wedding", value: "wedding" },
+  {
+    categoryId: 3,
+    categoryName: "Special Occasion",
+    value: "special-occasion",
+  },
+  { categoryId: 4, categoryName: "Nature", value: "nature" },
+  { categoryId: 5, categoryName: "Travel", value: "travel" },
+  { categoryId: 6, categoryName: "Commercial", value: "commercial" },
+  {
+    categoryId: 7,
+    categoryName: "Corporate Events",
+    value: "corporate-events",
+  },
+  { categoryId: 8, categoryName: "Fashion & Portfolio", value: "fashion" },
+];
---
+++ src/assets/mock-data/cities.js
@@ -0,0 +1,11 @@
+export const cities = [
+  { cityId: 0, cityName: "All" },
+  { cityId: 1, cityName: "Mumbai" },
+  { cityId: 2, cityName: "Pune" },
+  { cityId: 3, cityName: "Delhi" },
+  { cityId: 4, cityName: "Bengaluru" },
+  { cityId: 5, cityName: "Hyderabad" },
+  { cityId: 6, cityName: "Chennai" },
+  { cityId: 7, cityName: "Ahmedabad" },
+  { cityId: 8, cityName: "Kolkata" },
+];
